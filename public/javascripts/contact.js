4f5f80ad895b6e737aca429016946b0a406fd863
Mahmud Ahmad <mahmudahmad@Mahmuds-MacBook-Pro.local>
2017-12-05 15:51:59 -0500

    getting post body?

--- public/javascripts/contact.js
+++ public/javascripts/contact.js
@@ -1,14 +1,14 @@
 $(function() {
-  $('.send-emails-button').on('click', sendEmails);
+    $('.send-emails-button').on('click', sendEmails);
 });
 
 function sendEmails() {
-  $.ajax({
-    type: "POST",
-    url: "send-mail",
-    data: undefined,
-    dataType: 'json',
-    success: function() { console.log('success'); },
-    error: function() { console.log('fail'); },
-  });
+    $.ajax({
+        type: "POST",
+        url: "send-mail",
+        data: undefined,
+        dataType: 'json',
+        success: function() { console.log('success'); },
+        error: function() { console.log('fail'); },
+    });
 }
